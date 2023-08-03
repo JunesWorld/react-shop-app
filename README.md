4b83eca407469755fb83be34afe2f65dfbcc245a
June <sungj1207@gmail.com>
2023-08-03 14:47:53 +0900

    Form 컴포넌트 생성하기 (React-Hook-Form)

--- README.md
+++ README.md
@@ -6,4 +6,5 @@
 npm init vite (./ + React + JS)
 npm install
 npm i axios @reduxjs/toolkit react-icons react-loading-skeleton sass react-router-dom
+npm i react-hook-form
 ```
--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -1,8 +1,64 @@
 import React from 'react'
+import styles from './Form.module.scss'
+import { useForm } from 'react-hook-form'
 
-const Form = () => {
+const Form = ({ title }) => {
+
+  const { register, handleSubmit, formState: { errors }, reset } = useForm({
+    mode: 'onBlur'
+  })
+
+  const onSubmit = ({ email, password }) => {
+    console.log(email, password)
+  }
+
+  const userEmail = {
+    required: "필수 필드입니다."
+  }
+
+  const userPassword = {
+    required: "필수 필드입니다.",
+    minLength: {
+      value: 4,
+      message: "최소 4자입니다."
+    }
+  }
+
+
   return (
-    <div>Form</div>
+    <form onSubmit={handleSubmit()} className={styles.form}>
+      <div>
+        <input
+          type="email"
+          placeholder="E-mail"
+          {...register("email", userEmail)}
+        />
+        {errors?.email &&
+          <div>
+            <span className={styles.form_error}>
+              {errors.email.message}
+            </span>
+          </div>
+        }
+      </div>
+
+      <div>
+        <input
+          type="password"
+          placeholder="Password"
+          {...register("password", userPassword)}
+        />
+        {errors?.password &&
+          <div>
+            <span className={styles.form_error}>
+              {errors.password.message}
+            </span>
+          </div>
+        }
+      </div>
+      <button type='submit'>{title}</button>
+      <span className={styles.form_error}></span>
+    </form>
   )
 }
 
