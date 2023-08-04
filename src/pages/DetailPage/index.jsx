b2638f8e4350f836fca5787826f834c4efe0ff3a
June <sungj1207@gmail.com>
2023-08-04 17:02:05 +0900

    상세 페이지 UI 생성하기

--- src/pages/DetailPage/index.jsx
+++ src/pages/DetailPage/index.jsx
@@ -1,6 +1,18 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch } from 'react-redux';
+import { useParams } from 'react-router-dom'
+import { fetchProduct } from '../../store/products/product.slice';
 
 const DetailPage = () => {
+
+  const { id } = useParams();
+  const productId = Number(id);
+  const dispatch = useDispatch();
+  
+  useEffect(() => {
+    dispatch(fetchProduct(productId))
+  }, [])
+
   return (
     <div>DetailPage</div>
   )
