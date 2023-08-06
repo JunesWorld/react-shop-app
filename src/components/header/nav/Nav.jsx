664bd3fb7ff286640529275b4fb1467ef30375f4
June <sungj1207@gmail.com>
2023-08-06 17:40:33 +0900

    NavCartBlock 컴포넌트 생성하기

--- src/components/header/nav/Nav.jsx
+++ src/components/header/nav/Nav.jsx
@@ -10,6 +10,7 @@ import { useDispatch } from 'react-redux'
 import { removeUser } from '../../../store/user/user.slice'
 import { removeUserId } from '../../../store/cart/cart.slice'
 import { useAppSelector } from '../../../hooks/redux'
+import NavCartBlock from './nav-cart-block/NavCartBlock'
 
 const Nav = () => {
   const { isAuth } = useAuth()
@@ -37,6 +38,10 @@ const Nav = () => {
               <FiShoppingCart />
             </Link>
             {products.length > 0 && <b>{products.length}</b>}
+            {products.length > 0 && 
+              <div className={styles.nav_hover_cart}>
+                <NavCartBlock />
+              </div>}
           </div>
         </li>
         <li>
