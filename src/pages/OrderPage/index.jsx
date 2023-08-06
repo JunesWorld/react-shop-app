8308cb017a9f96cd69ba2df243f4b5770c137e99
June <sungj1207@gmail.com>
2023-08-06 17:01:29 +0900

    Order List 컴포넌트 생성하기

--- src/pages/OrderPage/index.jsx
+++ src/pages/OrderPage/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useAuth } from '../../hooks/useAuth'
 import { Navigate } from 'react-router-dom'
+import OrdersList from './orders-list/OrdersList'
 
 const OrderPage = () => {
 
@@ -12,6 +13,7 @@ const OrderPage = () => {
     <div className='page'>
       <div className='container'>
         <h1>주문 히스토리</h1>
+        <OrdersList />
       </div>
     </div>
   )
